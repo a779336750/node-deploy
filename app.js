f6fcb82b75f19323365389deb9933c09cae46844
jason <779336750@qq.com>
2020-06-28 14:02:50 +0800

    新增pm2进程管理日志

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var uploadRouter = require('./routes/upload');
-
 var app = express();
 
 // view engine setup
@@ -28,16 +27,13 @@ app.use('/upload', uploadRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-
 module.exports = app;
